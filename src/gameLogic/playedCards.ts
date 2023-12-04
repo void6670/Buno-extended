eea5bf17e914ccee9d4f4aaa1814806cd4376167
GeekCornerGH <45696571+GeekCornerGH@users.noreply.github.com>
2023-12-04 09:39:24 +0100

    feat: Working Buno transfer when 0 played

--- src/gameLogic/playedCards.ts
+++ src/gameLogic/playedCards.ts
@@ -253,6 +253,12 @@ You drew ${cardEmotes[newCards[0]]}`,
 
                 const keys = Object.keys(game.cards);
                 keys.unshift(keys.pop());
+                for (let i = 0; i < keys.length; i++) {
+                    if (game.unoPlayers.includes(keys[i])) {
+                        game.unoPlayers.splice(game.unoPlayers.indexOf(keys[i]), 1);
+                        game.unoPlayers.push(keys[(i + 1) % keys.length]);
+                    }
+                }
                 game.cards = Object.fromEntries(Object.entries(game.cards).map(([_, value], i) =>
                     [keys[i], makeDrawCardProxy(value, game, keys[i])]
                 ));
